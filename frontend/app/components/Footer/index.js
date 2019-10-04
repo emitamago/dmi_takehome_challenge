7d7b72d3dd396c5cf488f6956050394354bd5d7d
Emi Tsukuda <gioramlevi13@gmail.com>
2019-10-03 23:52:10 -0700

    Updated footer

--- frontend/app/components/Footer/index.js
+++ frontend/app/components/Footer/index.js
@@ -10,16 +10,10 @@ function Footer() {
   return (
     <Wrapper>
       <section>
-        <FormattedMessage {...messages.licenseMessage} />
-      </section>
-      <section>
-        <LocaleToggle />
-      </section>
-      <section>
         <FormattedMessage
           {...messages.authorMessage}
           values={{
-            author: <A href="https://twitter.com/mxstbr">Max Stoiber</A>,
+            author: <A href="https://github.com/emitamago">Emi Tsukuda</A>,
           }}
         />
       </section>
